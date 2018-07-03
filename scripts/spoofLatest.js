02553e21aa60ffddbbbf9557076e2932cb2a6750
Mark Rofail <markm.rofail@gmail.com>
2018-07-03 16:16:57 +0200

    fix: add DEBUG const to spoofLatest script

--- scripts/spoofLatest.js
+++ scripts/spoofLatest.js
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+// HYPER PARAMETERS                                                          //
+///////////////////////////////////////////////////////////////////////////////
+DEBUG = false
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Script                                                                    //
+///////////////////////////////////////////////////////////////////////////////
 admin.addPeer("enode://cb1aa2d0aa1afbae08d582be88f11fbb0feb4b9c90aa5613e4de55b4a388514d120408ee2533739bee9034eae3cd5a5757a7e52733d1c0822226fc1ec0ac8f90@134.60.77.152:30303")
 console.log('[INFO]\t', 'peer added')
 
@@ -13,6 +22,8 @@ console.log('[INFO]\t', 'head retrived\t', 'block number:', head.number)
 result = miner.commitSpoofedWork(head.parentHash, head.timestamp)
 console.log('[INFO]\t', 'spoof mined\t', 'block hash:', result)
 
-// block = eth.getBlock(result)
-// console.log('[DEBUG]\t', 'mined block details:')
-// console.log(JSON.stringify(block, null, "    "))
+if(DEBUG){
+    block = eth.getBlock(result)
+    console.log('[DEBUG]\t', 'mined block details:')
+    console.log(JSON.stringify(block, null, "    "))    
+}
